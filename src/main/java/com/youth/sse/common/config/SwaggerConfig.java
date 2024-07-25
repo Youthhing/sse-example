8b2947b58d987d5f698a8ce9fd954f4f3596ecca
youth <boysoeng@naver.com>
2024-07-25 15:43:11 +0900

    chore: swagger setting

---
+++ src/main/java/com/youth/sse/common/config/SwaggerConfig.java
@@ -0,0 +1,26 @@
+package com.youth.sse.common.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@OpenAPIDefinition(
+        info = @Info(title = "스웨거 API 명세서",
+                description = "YOUTH_DEV_SWAGGER 명세서",
+                version = "v1"))
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public GroupedOpenApi chatOpenApi() {
+        // "/v1/**" 경로에 매칭되는 API를 그룹화하여 문서화한다.
+        String[] paths = {"/api/**"};
+
+        return GroupedOpenApi.builder()
+                .group("스웨거 테스트 API v1")  // 그룹 이름을 설정한다.
+                .pathsToMatch(paths)     // 그룹에 속하는 경로 패턴을 지정한다.
+                .build();
+    }
+}
